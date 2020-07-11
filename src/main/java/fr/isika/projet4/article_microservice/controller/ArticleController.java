e3f417df0220e836f694a25a317b9e9f08dde042
Cholay <michael.cholay@gmail.com>
2020-07-11 03:26:23 +0200

    add cors annotation

--- src/main/java/fr/isika/projet4/article_microservice/controller/ArticleController.java
+++ src/main/java/fr/isika/projet4/article_microservice/controller/ArticleController.java
@@ -3,6 +3,7 @@ package fr.isika.projet4.article_microservice.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Api("API for get requests of articles")
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/article-api/public")
 public class ArticleController {
